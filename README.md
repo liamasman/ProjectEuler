c04801838d2bce131adfb667f4284116c8d9d238
Liam Asman <Liam@LiamAsman.com>
2025-12-07 20:51:01 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Project Euler
+
+Implementations for solving Project Euler problems.
+
+## How to build
+
+Run
+
+```shell
+./gradlew installDist
+```
+
+## How to run
+
+Run the generated script:
+
+```shell
+build/install/project-euler/bin/project-euler <problem number>
+```
---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+group = "com.liamasman"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass = "com.liamasman.Main"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
