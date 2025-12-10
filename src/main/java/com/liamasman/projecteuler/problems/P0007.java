536217a6bb09651337e9499bb3282ccce2ce4ad9
Liam Asman <Liam@LiamAsman.com>
2025-12-10 19:11:52 +0000

    Add problem 7

---
+++ src/main/java/com/liamasman/projecteuler/problems/P0007.java
@@ -0,0 +1,23 @@
+package com.liamasman.projecteuler.problems;
+
+import com.liamasman.projecteuler.framework.annotation.Problem;
+import com.liamasman.projecteuler.framework.annotation.ProblemCase;
+import com.liamasman.projecteuler.framework.annotation.TestCase;
+import com.liamasman.projecteuler.util.SieveOfEratosthenes;
+
+import java.util.stream.StreamSupport;
+
+public class P0007 {
+    @Problem(id = 7)
+    @ProblemCase(input = "10001")
+    @TestCase(input = "6", solution = "13")
+    public long tenThousandAndFirstPrime(final String[] input) {
+        final var limit = Integer.parseInt(input[0]);
+
+        final var sieve = new SieveOfEratosthenes();
+        return StreamSupport.stream(sieve.spliterator(), false)
+                .skip(limit - 1)
+                .findFirst()
+                .get();
+    }
+}
